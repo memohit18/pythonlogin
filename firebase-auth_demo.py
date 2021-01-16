6a1741ee9a48e0f90f107a1ba347968e6a17ac00
mohit <mohit2010sm@gmail.com>
2021-01-16 15:47:04 +0530

    forgot and reset added

--- firebase-auth_demo.py
+++ firebase-auth_demo.py
@@ -1,4 +1,5 @@
 import pyrebase
+from getpass import getpass
 
 firebaseConfig = {'apiKey': "AIzaSyBgXyDzMyO2EG3n1FhUDp9m9uacnnHlf6Y",
     'authDomain': "authpython-b780b.firebaseapp.com",
@@ -12,29 +13,45 @@ firebaseConfig = {'apiKey': "AIzaSyBgXyDzMyO2EG3n1FhUDp9m9uacnnHlf6Y",
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
 
+def __init__(self,api_key):
+    self.api_key = api_key
+    self.current_user = None
+    self.credentials = credentials
+
 def signUp():
     print("Signing Up...")
     email = input("Enter Email: ")
-    password =input("Enter Password: ")
+    password = getpass("Enter Password: ")
+    user = auth.create_user_with_email_and_password(email,password)
+    auth.send_email_verification(user['idToken'])
+    print('email verification link send')
     try:
-        user = auth.create_user_with_email_and_password(email,password)
         print("New Account Created!")
         ask = input("Do you want to Login now?[y/n]")
         if ask == 'y':
             login()
     except:
         print("Email already Exists")
+        ask = input("Log in?[y/n]")
+        if ask == 'y':
+            login()
 
 def login():
     print("Logging In...")
     email = input("Enter Email: ")
-    password =input("Enter Password: ")
+    password =getpass("Enter Password: ")
     try:
         login = auth.sign_in_with_email_and_password(email,password)
         print("Successfully Logged In")
         print(auth.get_account_info(login['idToken']))
     except:
-        print("wrong password")
+        print("Invalid password")
+        ask = input("do you want to reset password?[y/n]")
+        if ask == 'y':
+            auth.send_password_reset_email(email)
+            print('Reset password link send')
+        
+      
 
 ans=input("Are you a new user[y/n]")
 if ans =='y':
